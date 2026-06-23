6dd9e22a70b7231ef2f0d3eef6a720995ebc362b
Julian Hamre <julian.hamre5@gmail.com>
2026-06-23 12:57:14 +0200

    Change smart splits nvim config

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -3,3 +3,13 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("i", "jk", "<ESC>", { silent = true })
+
+local ss = require("smart-splits")
+vim.keymap.set({ "n", "v", "t" }, "<C-h>", ss.move_cursor_left, { desc = "Move to left pane" })
+vim.keymap.set({ "n", "v", "t" }, "<C-j>", ss.move_cursor_down, { desc = "Move to lower pane" })
+vim.keymap.set({ "n", "v", "t" }, "<C-k>", ss.move_cursor_up, { desc = "Move to upper pane" })
+vim.keymap.set({ "n", "v", "t" }, "<C-l>", ss.move_cursor_right, { desc = "Move to right pane" })
+vim.keymap.set({ "n", "v" }, "<C-Left>", ss.resize_left, { desc = "Resize pane left" })
+vim.keymap.set({ "n", "v" }, "<C-Down>", ss.resize_down, { desc = "Resize pane down" })
+vim.keymap.set({ "n", "v" }, "<C-Up>", ss.resize_up, { desc = "Resize pane up" })
+vim.keymap.set({ "n", "v" }, "<C-Right>", ss.resize_right, { desc = "Resize pane right" })
