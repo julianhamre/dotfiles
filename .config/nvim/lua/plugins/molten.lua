866c47b4b31c5d5d7e322e7e41f6a70f531083f6
Julian Hamre <julian.hamre5@gmail.com>
2026-06-30 23:29:13 +0200

    Improve molten keybinds

--- .config/nvim/lua/plugins/molten.lua
+++ .config/nvim/lua/plugins/molten.lua
@@ -4,13 +4,13 @@ return {
     dependencies = { "3rd/image.nvim" },
     build = ":UpdateRemotePlugins",
     init = function()
-        -- these are examples, not defaults. Please see the readme
         vim.g.molten_image_provider = "image.nvim"
         vim.g.molten_output_win_max_height = 20
+        require("which-key").add({ { "<leader>m", group = "molten", icon = "⚗️" } })
     end,
     keys = {
         -- session
-        { "<leader>mi", ":MoltenInit<CR>", desc = "Molten: init kernel" },
+        { "<leader>mI", ":MoltenInit<CR>", desc = "Molten: init kernel" },
         { "<leader>mq", ":MoltenInterruptKernel<CR>", desc = "Molten: interrupt kernel" },
         { "<leader>mR", ":MoltenRestartKernel<CR>", desc = "Molten: restart kernel" },
 
@@ -19,19 +19,16 @@ return {
         { "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>", mode = "v", desc = "Molten: eval selection" },
         { "<leader>mr", ":MoltenReevaluateCell<CR>", desc = "Molten: re-eval cell" },
         { "<leader>mf", function()
-            vim.cmd("normal! ggVG")
-            vim.cmd("MoltenEvaluateVisual")
+            vim.fn.MoltenEvaluateRange(1, vim.fn.line("$"))
           end, desc = "Molten: eval file" },
         { "<leader>ma", function()
-            local line = vim.fn.line(".")
-            vim.cmd("normal! ggV" .. line .. "G")
-            vim.cmd("MoltenEvaluateVisual")
+            vim.fn.MoltenEvaluateRange(1, vim.fn.line("."))
           end, desc = "Molten: eval top to cursor" },
 
         -- output
         { "<leader>mo", ":noautocmd MoltenEnterOutput<CR>", desc = "Molten: enter output" },
         { "<leader>mh", ":MoltenHideOutput<CR>", desc = "Molten: hide output" },
-        { "<leader>mI", ":MoltenImagePopup<CR>", desc = "Molten: image popup" },
+        { "<leader>mi", ":MoltenImagePopup<CR>", desc = "Molten: image popup" },
         { "<leader>md", ":MoltenDelete<CR>", desc = "Molten: delete cell" },
 
         -- navigation
