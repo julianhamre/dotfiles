a6124a79061ee4412e2ff5d13c12b19ab1516112
Julian Hamre <julian.hamre5@gmail.com>
2026-07-19 11:45:16 +0200

    Use the tokyonight moon color scheme exclusively

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 local wezterm = require("wezterm")
 return {
-	color_scheme = "Catppuccin Mocha",
-	font = wezterm.font("JetBrainsMono Nerd Font"),
+  font = wezterm.font("JetBrainsMono Nerd Font"),
 	enable_tab_bar = false,
 }
