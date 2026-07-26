86059428105a9f7cb8d8e9c2916fa114c2f031f1
Julian Hamre <julian.hamre5@gmail.com>
2026-07-26 19:55:04 +0200

    Enable ltex-ls dictionary code actions
    Dictionary code actions like "add to dictionary"
    now work properly after configuring the ltex_extra
    plugin. The ltex dictionaries are local to each
    project by default.

--- .config/nvim/lua/plugins/writing.lua
+++ .config/nvim/lua/plugins/writing.lua
@@ -1,30 +1,39 @@
+local GLOBAL_DICT_PATH = vim.fn.stdpath("config") .. "/spell"
+
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      ltex_plus = {
-        filetypes = { "markdown", "text", "tex", "plaintex", "bib", "gitcommit", "quarto", "rmd" },
-        settings = {
-          ltex = {
-            language = "en-US",
-            additionalRules = {
-              enablePickyRules = true,
-              -- languageModel = vim.fn.expand("~/.local/share/ltex/ngrams/"),  -- if you download n-grams
-            },
-            dictionary = {
-              ["en-US"] = { ":" .. vim.fn.stdpath("config") .. "/spell/ltex.en.txt" },
+  {
+    "barreiroleo/ltex_extra.nvim",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        ltex_plus = {
+          filetypes = { "markdown", "text", "tex", "plaintex", "bib", "gitcommit", "quarto", "rmd" },
+          on_attach = function(client, _)
+            require("ltex_extra").setup({
+              load_langs = { "en-US" },
+              path = client.config.root_dir or GLOBAL_DICT_PATH,
+            })
+          end,
+          settings = {
+            ltex = {
+              language = "en-US",
+              additionalRules = {
+                enablePickyRules = true,
+              },
             },
           },
         },
-      },
-      harper_ls = {
-        filetypes = {
-          "python", "r", "lua", "rust", "c", "cpp",
-          "java", "javascript", "typescript", "go", "sh",
-        },
-        settings = {
-          ["harper-ls"] = {
-            userDictPath = vim.fn.stdpath("config") .. "/spell/harper.txt",
+        harper_ls = {
+          filetypes = {
+            "python", "r", "lua", "rust", "c", "cpp",
+            "java", "javascript", "typescript", "go", "sh",
+          },
+          settings = {
+            ["harper-ls"] = {
+              userDictPath = GLOBAL_DICT_PATH .. "/harper.txt",
+            },
           },
         },
       },
