d167a0ee582079ba68f01d3dc5adbb6e95539de5
Julian Hamre <julian.hamre5@gmail.com>
2026-09-02 08:59:34 +0200

    Turn off spelling in md files

--- .config/nvim/lua/plugins/markdown.lua
+++ .config/nvim/lua/plugins/markdown.lua
@@ -1,3 +1,12 @@
+vim.schedule(function()
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function()
+      vim.opt_local.spell = false
+    end,
+  })
+end)
+
 return {
   "mfussenegger/nvim-lint",
   opts = {
